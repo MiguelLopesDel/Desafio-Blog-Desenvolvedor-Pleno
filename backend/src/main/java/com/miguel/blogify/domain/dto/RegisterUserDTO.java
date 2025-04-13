68339ce74c9ae9961895d3ff10f9090eb72a5363
gamerYToff <miguellopesdelmondes2@gmail.com>
2025-04-13 18:25:21 -0300

    Add name field to RegisterUserDTO and User, and enhance UserNotFoundException message

--- backend/src/main/java/com/miguel/blogify/domain/dto/RegisterUserDTO.java
+++ backend/src/main/java/com/miguel/blogify/domain/dto/RegisterUserDTO.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size;
 
 public record RegisterUserDTO(@Email(message = "Email deve ser válido")
                               @NotBlank(message = "Email não pode estar em branco") String email,
+                              String name,
                               @NotBlank(message = "Senha não pode estar em branco")
                               @Size(min = 8, message = "Senha deve ter no mínimo 8 caracteres") String password) {
 }
